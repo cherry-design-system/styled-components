2f3db18fbfe7e3de2705470300ca1cfd0cf702fe
Luan Gjokaj <luan@riangle.com>
2024-03-16 05:04:54 +0100

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import { Metadata } from "next";
+import {
+	Button,
+	Col,
+	Container,
+	Flex,
+	Grid,
+	Input,
+	MaxWidth,
+	Range,
+	Select,
+	Space,
+	Textarea,
+	Toggle,
+} from "./components";
 
 export const metadata: Metadata = {
 	title: "Cherry Design System",
@@ -7,8 +21,78 @@ export const metadata: Metadata = {
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Cherry</h1>
-		</div>
+		<Container>
+			<Space $size={100} />
+			<MaxWidth $xs={845}>
+				<Grid $xsCols={1} $lgCols={3} $gap={20}>
+					<Col $xsSpan={1} $lgSpan={2}>
+						<Grid $xsCols={1} $lgCols={2} $gap={20}>
+							<Input placeholder="Placeholder" $fullWidth />
+							<Select $fullWidth>
+								<option>Select</option>
+							</Select>
+							<Col $lgSpan={2}>
+								<Flex
+									$xsGap={10}
+									$lgGap={20}
+									$wrap="nowrap"
+									$justifyContent="space-between"
+								>
+									<Input type="checkbox" id="checkbox-1" />
+									<Input
+										type="checkbox"
+										id="checkbox-2"
+										defaultChecked
+									/>
+									<Input
+										type="radio"
+										id="radio-2"
+										name="demo-radio"
+									/>
+									<Input
+										type="radio"
+										id="radio-3"
+										name="demo-radio"
+										defaultChecked
+									/>
+									<Range />
+									<Toggle />
+									<Toggle defaultChecked />
+								</Flex>
+							</Col>
+						</Grid>
+					</Col>
+					<Textarea $fullWidth defaultValue="Textarea" />
+				</Grid>
+				<Space $size={20} />
+				<Grid $xsCols={1} $lgCols={2} $gap={20}>
+					<Col>
+						<Flex $wrap="nowrap" $gap={20}>
+							<Button $fullWidth>Button</Button>
+							<Button $variant="secondary" $fullWidth>
+								Button
+							</Button>
+							<Button $variant="tertiary" $fullWidth>
+								Button
+							</Button>
+						</Flex>
+					</Col>
+					<Col>
+						<Flex $wrap="nowrap" $gap={20}>
+							<Button $outline $fullWidth>
+								Button
+							</Button>
+							<Button $variant="secondary" $outline $fullWidth>
+								Button
+							</Button>
+							<Button $variant="tertiary" $outline $fullWidth>
+								Button
+							</Button>
+						</Flex>
+					</Col>
+				</Grid>
+			</MaxWidth>
+			<Space $size={100} />
+		</Container>
 	);
 }
