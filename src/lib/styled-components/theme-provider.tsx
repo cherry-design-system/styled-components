e76494e57b4d251344a466410e986a0209b03104
Luan Gjokaj <luan@riangle.com>
2024-03-19 01:57:59 +0100

    feat: required theme prop

--- src/lib/styled-components/theme-provider.tsx
+++ src/lib/styled-components/theme-provider.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { GlobalStyles } from "../utils";
+import { GlobalStyles, Theme } from "../utils";
 
 function CherryThemeProvider({
 	children,
 	theme,
 }: {
 	children: any;
-	theme?: any;
+	theme: Theme;
 }) {
 	const GlobalStylesComponent = GlobalStyles(theme);
 
